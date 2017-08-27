32fddb77e40468d4d2ffc39593eb2361ed2472a8
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-27 19:51:44 +0400

    added NForge nuget package

---
+++ NeuralNetworks/Perceptron/Program.cs
@@ -0,0 +1,17 @@
+﻿using AForge.Neuro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new ActivationNetwork(new )
+        }
+    }
+}
